b0242190ecea5b7fcfb3c4bb61e77593f0db6c89
Ferris Boran <ferris.boran@gmail.com>
2019-03-11 19:58:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+	last = []
+	if array.length == 1
+		return array.join
+	elsif array.length == 2
+		return array.join(' and ')
+	else
+		last << "and #{array.pop}"
+		array.concat(last)
+		return array.join(', ')
+	end
 end
