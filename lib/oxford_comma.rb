b7c5a5b61c308672de4f96d122ca7517f6c2e3c1
L D <tapircode@gmail.com>
2019-03-12 03:13:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  else if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    result = ""
+    for i in 0...(array.length-1)
+      result += "#{array[i]}, "
+    end
+    result += "and #{array[array.length-1]}"
+    return result
+  end
+end
 end
