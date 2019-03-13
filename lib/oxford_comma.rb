90e16d6aaed1989e1fba759c1d36a3cad99eecfc
Edgar Ong <emailedgar@gmail.com>
2019-03-13 18:58:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+#join will stitch items together
+#split will split items into an array
+#until loop reaches the last array item, make new array add last time with 'and'
+#i want ["kiwi", "strawberries"] to be "kiwi and strawberries"
+# ["kiwi", "strawberries", "bananas"] to be "kiwi, strawberries, and bananas"
 
+def oxford_comma(array)
+  
+  if array.size == 1
+    array.join
+    elsif array.size == 2
+    array.join(" and ")
+    elsif array.size >= 3
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
+    
 end
