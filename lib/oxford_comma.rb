a2e99a04ab4afc388d1bbf970bd7295093b3d69d
danfyfe <fyfedaniel@gmail.com>
2019-03-21 16:16:15 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
-
-end
+  def oxford_comma(array)
+    if array.size > 2
+      index = 0
+      until index == array.size-1
+        array[index] = "#{array[index]}, "
+        index += 1
+      end
+      array[-1] = "and #{array[-1]}"
+    elsif array.size == 2
+      array[-1] = " and #{array[-1]}"
+    end
+    array.join
+  end
