a2065f6b470cc1c351943cd9185e06e9c6cbce4a
Arslan Ali <ali.arslan319@gmail.com>
2019-03-26 23:27:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
+array = ["kiwi"]
 
+def oxford_comma(array)
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+    array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+  array.join(", ")
+  end
 end
+
+
+#
+# describe "#oxford_comma" do
+#   it 'returns a string without any additional fomatting when given a 1-element array' do
+#     expect(oxford_comma(["kiwi"])).to eq("kiwi")
+#   end
