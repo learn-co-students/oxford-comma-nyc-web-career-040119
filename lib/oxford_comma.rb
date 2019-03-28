0fe355d6eb0fce851a2f736c4cec161ba143b4e5
Jennifer Busca <jennifer.busca@gmail.com>
2019-03-28 20:09:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+  new_arr = []
+  string = ""
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    string = array.join(" and ")
+  elsif array.length == 3
+    new_arr << array[0] + ","
+    new_arr << array[1] + ", and"
+    new_arr << array[-1]
+    new_arr.join(" ")
+  elsif array.length > 3
+    array[0..-2].each do |ele | 
+      new_arr << ele + ","
+    end
+    new_arr.insert(-1, "and")
+    new_arr << array[-1]
+    string = new_arr.join(" ")
+  end
 end
